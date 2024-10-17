6a3d746d599d58506a1c2f9633d4b430641a1831
Musa Zeshan <musazeshan.it@gmail.com>
2024-10-17 12:18:06 +0500

    The hero section is now complete.

---
+++ app/components/Hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import HeroImg from '../../public/hero.png';
+import Arrow from '../../public/arrow.png';
+
+const Hero = () => {
+    return (
+        <div className='mb-20 px-16 py-4 h-fit bg-[rgb(245,247,246)] w-full'>
+            <div className='flex justify-between relative'>
+                <div>
+                    <h1 className='mt-20 text-[64px] font-bold leading-tight'>
+                        Hey, I am
+                        <br />
+                        Zeshan Qaiser
+                        <br />
+                        <div className='w-full overflow-hidden relative'>
+                            Graphic Designer
+                            <svg width="495" height="18" viewBox="0 0 495 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_704_1264)">
+                                    <path d="M5 13C90.8746 6.96301 308.099 -1.48877 490 13" stroke="#DAED1A" strokeWidth="10" strokeLinecap="round" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_704_1264">
+                                        <rect width="495" height="18" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                    </h1>
+                    <p className='text-gray-500 text-2xl mt-5'>Agency work with top-rated talented people that provide the best services.</p>
+                    <button className='flex justify-center items-center gap-x-2 bg-blue-600 text-white rounded-lg px-6 py-4 mt-8' type='button'>
+                        Hire me
+                        <FontAwesomeIcon className='w-4 h-4' icon={faArrowRightLong} />
+                    </button>
+                </div>
+                <Image className='h-[600px]' src={HeroImg} alt="Hero Image" />
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowRightLong, faCodeCompare, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { faFacebook, faFacebookF, faInstagram, faLinkedin, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const Navbar = () => {
+    return (
+        <div className='py-4 px-16 bg-[rgb(245,247,246)]'>
+            <div className="flex items-center justify-between">
+                <div>
+                    <div className='flex gap-x-10'>
+                        <div className='flex gap-x-2 items-center'>
+                            <FontAwesomeIcon className='text-blue-500 h-4' icon={faEnvelope} />
+                            <p className='text-gray-500'>zeshanqaiser.it@gmail.com</p>
+                        </div>
+                        <div className='flex gap-x-2 items-center'>
+                            <FontAwesomeIcon className='text-blue-500 h-4' icon={faPhone} />
+                            <p className='text-gray-500'>+92 307-9410053</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex gap-x-6 text-gray-500'>
+                    <p className='h-4 w-4'><FontAwesomeIcon icon={faFacebook} /></p>
+                    <p className='h-4 w-4'><FontAwesomeIcon icon={faInstagram} /></p>
+                    <p className='h-4 w-4'><FontAwesomeIcon icon={faTwitter} /></p>
+                    <p className='h-4 w-4'><FontAwesomeIcon icon={faLinkedinIn} /></p>
+                </div>
+            </div>
+            <div className='w-full mt-3 bg-white shadow-[0_0_20px_0.1px_rgba(0,0,0,0.05)] rounded-xl'>
+                <div className='flex justify-between items-center p-4'>
+                    <div className='flex gap-x-2 items-center'>
+                        <div className='bg-blue-600 rounded-full w-11 h-11 flex justify-center items-center'>
+                            <FontAwesomeIcon className='text-white h-5' icon={faCodeCompare} />
+                        </div>
+                        <h1 className='text-[34px] tracking-[0.02em] font-bold'>fallow</h1>
+                    </div>
+                    <div className='flex gap-x-16 items-center'>
+                        <div className='flex gap-x-10 text-[17px]'>
+                            <h3 className='font-medium text-gray-800'>Services</h3>
+                            <h3 className='font-medium text-gray-800'>Contact</h3>
+                            <h3 className='font-medium text-gray-800'>About</h3>
+                            <h3 className='font-medium text-gray-800'>Work</h3>
+                            <h3 className='font-medium text-gray-800'>Portfolio</h3>
+                        </div>
+                        <div>
+                            <button className='flex gap-x-2 font-medium items-center bg-blue-600 rounded-lg px-5 py-[13px] text-white'>
+                                Let's Talk
+                                <FontAwesomeIcon className="h-4" icon={faArrowRightLong} />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
