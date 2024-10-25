048643294e8c01f5d0a6c901139131addf34431a
Musa Zeshan <musazeshan.it@gmail.com>
2024-10-25 22:13:10 +0500

    The hero section has also been made completely responsive.

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -7,16 +7,12 @@ import Arrow from '../../public/arrow.png';
 
 const Hero = () => {
     return (
-        <div className='2xl:px-20 xl-custom:px-16 xl:px-14 lg-custom:px-12 lg:px-10 md-custom:px-32 md:px-28 sm-custom:px-24 sm:px-20 xs-custom:px-14 xs:px-10 2xs-custom:px-6 px-4 py-4 h-fit bg-[rgb(245,247,246)] w-full'>
+        <div className='2xl:px-20 xl-custom:px-16 xl:px-14 lg-custom:px-12 lg:px-10 md-custom:px-28 md:px-24 sm-custom:px-20 sm:px-16 xs-custom:px-12 xs:px-8 2xs-custom:px-6 px-4 py-4 h-fit bg-[rgb(245,247,246)] w-full'>
             <div className='flex lg:flex-row flex-col gap-10 justify-between'>
                 <div className='lg:w-6/12'>
-                    <h1 className='mt-20 xl:text-[64px] lg-custom:text-[60px] lg:text-[56px] md:text-[64px] sm:text-[60px] xs:text-[56px] 2xs-custom:text-[52px] 2xs:text-[48px] text-[44px] font-bold leading-tight'>
-                        Hey, I am
-                        <br />
-                        Zeshan Qaiser
-                        <br />
+                    <h1 className='mt-20 xl:text-[64px] lg-custom:text-[58px] lg:text-[54px] md:text-[50px] sm:text-[47px] xs:text-[45px] 2xs-custom:text-[42px] 2xs:text-[38px] text-[34px] font-bold leading-tight'>
+                        Hey, I am <br /> Zeshan Qaiser <br /> Graphic Designer
                         <div className='w-full overflow-hidden'>
-                            Graphic Designer
                             <svg width="495" height="18" viewBox="0 0 495 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g clipPath="url(#clip0_704_1264)">
                                     <path d="M5 13C90.8746 6.96301 308.099 -1.48877 490 13" stroke="#DAED1A" strokeWidth="10" strokeLinecap="round" />
@@ -29,7 +25,7 @@ const Hero = () => {
                             </svg>
                         </div>
                     </h1>
-                    <p className='text-gray-500 text-2xl mt-5'>Agency work with top-rated talented people that provide the best services.</p>
+                    <p className='text-gray-500 md:text-2xl sm:text-xl text-[18.5px] sm:mt-5 mt-4'>Agency work with top-rated talented people that provide the best services.</p>
                     <button className='flex justify-center items-center text-[17px] gap-x-2 bg-blue-600 text-white rounded-lg px-[22px] py-[14px] mt-8' type='button'>
                         Hire me
                         <FontAwesomeIcon className='w-4 h-4' icon={faArrowRightLong} />
